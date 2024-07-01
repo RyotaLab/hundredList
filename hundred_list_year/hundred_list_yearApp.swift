4b744c89c08dedae99cdbdc896aefb44c40167c4
RyotaLab <orengi12260@gmail.com>
2024-07-01 17:19:47 +0900

    Initial Commit

---
+++ hundred_list_year/hundred_list_yearApp.swift
@@ -0,0 +1,17 @@
+//
+//  hundred_list_yearApp.swift
+//  hundred_list_year
+//
+//  Created by 渡邊涼太 on 2024/07/01.
+//
+
+import SwiftUI
+
+@main
+struct hundred_list_yearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
